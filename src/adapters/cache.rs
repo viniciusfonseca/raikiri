5513c3445236d9e801f4b962bf591f4481d71ac8
Leandro Alves <leandroprogramadoruxdesign@gmail.com>
2024-10-05 22:55:41 -0300

    versao com build async

--- src/adapters/cache.rs
+++ src/adapters/cache.rs
@@ -30,6 +30,27 @@ impl<K, V> Cache<K, V>
         }
     }
 
+    pub async fn get_entry_by_key_async_build<F>(&self, key: K, build_default_if_new_async: F) -> Arc<RwLock<V>>
+        where F: std::future::Future<Output = V> {
+        let hs = self.hashes.read().await;
+        let found = hs.get(&key);
+
+        match found {
+            Some(entry) => {
+                return entry.clone();
+            }
+
+            None => {
+                drop(hs);
+                let mut write_hs = self.hashes.write().await;
+                let default_if_new = Arc::new(RwLock::new(build_default_if_new_async.await));
+                (*write_hs).insert(key, default_if_new.clone());
+                drop(write_hs);
+                return default_if_new;
+            }
+        }
+    }
+
     // It's Gracefully because if other thread still awaits for entry's write/read,
     // the entry neither the inside value are disposed, and the dispose only happens when the last thread disposes (Arc behavior).
 
