b9cf3a028057b375a86c0323a21ca51089a129ec
viniciusfonseca <vfonseca1618@gmail.com>
2025-04-13 17:11:26 -0300

    wip

--- crates/raikiri/src/domain/raikiri_env_db.rs
+++ crates/raikiri/src/domain/raikiri_env_db.rs
@@ -6,33 +6,35 @@ use tokio::sync::RwLock;
 use super::raikiri_env::{RaikiriEnvironment, ThreadSafeError};
 
 pub enum RaikiriDBConnectionKind {
-    ODBC,
+    PostgreSQL,
+    MySQL,
     MongoDB,
     DynamoDB
 }
 
 #[async_trait]
 pub trait RaikiriEnvironmentDB {
-    async fn create_connection(&self, kind: RaikiriDBConnectionKind) -> Box<dyn RaikiriDBConnection + Send + Sync>; 
-    async fn get_connection(&self, id: String, kind: RaikiriDBConnectionKind) -> Arc<RwLock<Box<dyn RaikiriDBConnection + Send + Sync>>>;
+    async fn create_connection(&self, kind: RaikiriDBConnectionKind, params: Vec<u8>) -> Box<dyn RaikiriDBConnection + Send + Sync>; 
+    async fn get_connection(&self, id: String) -> Arc<RwLock<Box<dyn RaikiriDBConnection + Send + Sync>>>;
 }
 
 #[async_trait]
 pub trait RaikiriDBConnection {
-    async fn execute_command(&self, params: Vec<u8>) -> Result<Vec<u8>, ThreadSafeError>;
+    async fn execute_command(&mut self, params: Vec<u8>) -> Result<Vec<u8>, ThreadSafeError>;
 }
 
 #[async_trait]
 impl RaikiriEnvironmentDB for RaikiriEnvironment {
-    async fn create_connection(&self, kind: RaikiriDBConnectionKind) -> Box<dyn RaikiriDBConnection + Send + Sync> {
+    async fn create_connection(&self, kind: RaikiriDBConnectionKind, params: Vec<u8>) -> Box<dyn RaikiriDBConnection + Send + Sync> {
         match kind {
-            RaikiriDBConnectionKind::ODBC => todo!(),
+            RaikiriDBConnectionKind::PostgreSQL => todo!(),
+            RaikiriDBConnectionKind::MySQL => todo!(),
             RaikiriDBConnectionKind::MongoDB => todo!(),
             RaikiriDBConnectionKind::DynamoDB => todo!(),
             _ => panic!("Unsupported database kind")
         }
     }
-    async fn get_connection(&self, id: String, kind: RaikiriDBConnectionKind) -> Arc<RwLock<Box<dyn RaikiriDBConnection + Send + Sync>>> {
-        self.db_connections.get_entry_by_key_async_build(id, async move { self.create_connection(kind).await }).await
+    async fn get_connection(&self, id: String) -> Arc<RwLock<Box<dyn RaikiriDBConnection + Send + Sync>>> {
+        self.db_connections.get_entry_by_key(id, || panic!("Connection not found")).await
     }
 }
